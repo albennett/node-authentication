d4f279c5762941079948afb8c57342cf74ddaaef
Amy Lee <amyleeannebennett@gmail.com>
2016-02-22 09:46:50 -0600

    added bodyParser

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 'use strict';
+
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'jade');
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.post('/login', (req, res) => {
+  res.redirect('/');
 })
 
 app.listen(PORT, () => {
