67d589450afd61f60ff6a974e7bbc5d30fd8fc05
Amy Lee <amyleeannebennett@gmail.com>
2016-02-23 09:57:57 -0600

    logout is working

--- server.js
+++ server.js
@@ -2,9 +2,11 @@
 
 const bodyParser = require('body-parser');
 const express = require('express');
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
+
 const userRoutes = require('./lib/user/routes');
 
 const app = express();
@@ -12,33 +14,31 @@ const PORT = process.env.PORT || 3000;
 const SESSION_SECRET = process.env.SESSION_SECRET || 'secret';
 
 app.set('view engine', 'jade');
-//middleware
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(session({
   secret: SESSION_SECRET,
   store: new RedisStore()
 }));
-app.use((req, res, next) => {
-  req.session.visits = req.session.visits || {};
-  req.session.visits[req.url] = req.session.visits[req.url] || 0;
-  req.session.visits[req.url]++
-
-  console.log(req.session);
-  next();
-});
+app.use(methodOverride('_method'));
 app.use(userRoutes);
+
+
+app.locals.title = '';
+
 app.use((req, res, next) => {
   res.locals.user = req.session.user || { email: 'Guest' };
   next();
 });
+
 app.get('/', (req, res) => {
-  res.render('index', {user: req.session.user});
+  res.render('index');
 });
 
-mongoose.connect('mongodb://localhost:27017/nodeauth', (err) =>{
+mongoose.connect('mongodb://localhost:27017/nodeauth', (err) => {
   if (err) throw err;
+
   app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
   });
-})
-
+});
